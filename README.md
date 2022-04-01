8ce0eb1c4eb7d6c8936cf112bb2d487ffc233ec5
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-04-01 11:26:10 +0200

    Added Tailwind

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Electron - React - Typescript
+# Electron - React - Typescript - Tailwind
 
 This is a ready-to-use template for coding `React` applications within
 `Electron`, with full `Typescript` support.
+
+The styling system is based on `TailwindCSS`.
 
 It uses `Yarn` as a package manager.
 
@@ -12,5 +14,11 @@ It uses `Yarn` as a package manager.
 	- it is referenced in `src/renderer.ts`, to pass it to the `Electron`
 	  renderer process
 - `src/App.tsx` contains the main application functional component, as usual
+- `src/index.css` contains the `Tailwind` rules
+- more `Tailwind properties can be found in:
+	- `tailwind.config.js`
+	- `postcss.config.js`
+		- `PostCSS` is also referenced in the `webpack.renderer.config.js`
+		   includes
 
 You can edit the title of the application in `src/index.html`.
--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React from "react"
 
 export default function App() {
     return(
-        <div className="App">
-            <h1>Electron - React - Typescript</h1>
-            <h2>Welcome!</h2>
-            <b>Start using this template by:</b>
-            <ul>
-                <li>editing the <i>App.tsx</i> page</li>
-                <li>creating React components with the <i>.tsx</i> extension</li>
-            </ul>
+        <div className="App h-screen w-screen bg-gray-700">
+            <div className="p-5 text-gray-100">
+                <h1 className="text-6xl text-bold text-red-500 text-center my-5">Electron - React - Typescript</h1>
+                <h2>Welcome!</h2>
+                <b>Start using this template by:</b>
+                <ul className="list-disc mx-5">
+                    <li>editing the <i>App.tsx</i> page</li>
+                    <li>creating React components with the <i>.tsx</i> extension</li>
+                    <li>adding <i>className="your-tailwind-properties"</i> to your JSX elements</li>
+                </ul>
+            </div>
         </div>
     )
 }
