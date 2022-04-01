e431cf566a2952a1391cb14750b04b702eb33f81
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-04-01 10:31:40 +0200

    First working version

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function App() {
+    return(
+        <div className="App">
+            <h1>Electron - React - Typescript</h1>
+            <h2>Welcome!</h2>
+            <b>Start using this template by:</b>
+            <ul>
+                <li>editing the <i>App.tsx</i> page</li>
+                <li>creating React components with the <i>.tsx</i> extension</li>
+            </ul>
+        </div>
+    )
+}
